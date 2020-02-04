a827bb8f46ca54a53263de598b09429e24d992e2
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2020-02-04 13:33:46 +0100

    a

--- Assets/Jan/JanScripts/Healthbehavior.cs
+++ Assets/Jan/JanScripts/Healthbehavior.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbehavior : MonoBehaviour, IDamagable
 {
     public int initialHp;
+    public int numOfHearts;
 
     int currentHp;
+
+    public Image[] hearts;
+    public Sprite fullHearts;
+    public Sprite emptyHearts;
 
     void Start()
     {
         currentHp = initialHp;
     }
+    
+    void Update()
+    {
+        if(currentHp > numOfHearts)
+        {
+            currentHp = numOfHearts;
+        }
 
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < currentHp)
+            {
+                hearts[i].sprite = fullHearts;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHearts;
+            }
+
+
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
     public void DoDamage(int amount)
     {
         currentHp -= amount;
