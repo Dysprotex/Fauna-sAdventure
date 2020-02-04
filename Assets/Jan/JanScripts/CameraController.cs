86b717c3ccb9f1963df9c39ac120a3f388c554a2
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2020-02-04 12:52:05 +0100

    a

--- Assets/Jan/JanScripts/CameraController.cs
+++ Assets/Jan/JanScripts/CameraController.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform lookAt;
+    public Transform camTransform;
+    private Camera cam;
+
+    Vector3 initalPos;
+    Vector3 originalPos;
+
     private const float Y_ANGLE_MIN = 5f;
     private const float Y_ANGLE_MAX = 80f;
 
     private const float DISTANCE_MIN = 10f;
     private const float DISTANCE_MAX = 50f;
 
-    public Transform lookAt;
-    public Transform camTransform;
-
-    private Camera cam;
 
     float distance = 20f;
     float currentX;
     float currentY;
     float sensivityX;
     float sensivityY;
+
+    public float intensity = 10f;
+    float shakeDuration;
+    bool isShaking;
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+        initalPos = camTransform.localPosition;
+    }
 
     private void Start()
     {
@@ -42,6 +58,11 @@ public class CameraController : MonoBehaviour
         {
             distance -= 1;
         }
+
+        if (shakeDuration > 0 && !isShaking)
+        {
+            StartCoroutine(DoShake());
+        }
     }
     private void LateUpdate()
     {
@@ -49,5 +70,28 @@ public class CameraController : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
         camTransform.position = lookAt.position + rotation * dir;
         camTransform.LookAt(lookAt.position);
+    }
+
+    public void Shake(float duration)
+    {
+        if (duration > 0)
+        {
+            shakeDuration += duration;
+        }
+    }
+
+    IEnumerator DoShake()
+    {
+        isShaking = true;
+        var startTime = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < startTime + shakeDuration)
+        {
+            var randomPoint = new Vector3(Random.Range(-1f, 1f) * intensity, Random.Range(-1f, 1f) * intensity, initalPos.z);
+            camTransform.localPosition = randomPoint;
+            yield return null;
+        }
+        shakeDuration = 0;
+        camTransform.localPosition = initalPos;
+        isShaking = false;
     }
 }
---
+++ Assets/Jan/JanScripts/CameraShakeer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeer : MonoBehaviour
+{
+    Transform target;
+    Vector3 initalPos;
+
+    public float intensity = 10f;
+    float shakeDuration;
+    bool isShaking;
+
+    void Start()
+    {
+        target = GetComponent<Transform>();
+        initalPos = target.localPosition;
+    }
+
+    public void Shake(float duration)
+    {
+        if(duration > 0)
+        {
+            shakeDuration += duration;
+        }
+    }
+
+    void Update()
+    {
+        if(shakeDuration > 0 && !isShaking)
+        {
+            StartCoroutine(DoShake());
+        }
+    }
+    IEnumerator DoShake()
+    {
+        isShaking = true;
+        var startTime = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < startTime + shakeDuration)
+        {
+            var randomPoint = new Vector3(Random.Range(-1f, 1f) * intensity, Random.Range(-1f, 1f) * intensity, initalPos.z);
+            target.localPosition = randomPoint;
+            yield return null;
+        }
+        shakeDuration = 0;
+        target.localPosition = initalPos;
+        isShaking = false;
+    }
+}
