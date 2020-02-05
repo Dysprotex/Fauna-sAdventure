fc5461c6dd6129b7b82c411ee79a7192d1e1c9fa
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2020-02-05 12:42:09 +0100

    MeineChanges

---
+++ Assets/Jan/JanScripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Time.timeScale = 0;
+        TriggerDialogue();
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
