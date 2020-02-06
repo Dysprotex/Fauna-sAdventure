1620ed681d55c9de6a7c43009214e45d05789d84
Dysprotex <58705183+Dysprotex@users.noreply.github.com>
2020-02-06 11:25:16 +0100

    a

---
+++ Assets/Jan/JanScripts/HealingEffekt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingEffekt : MonoBehaviour
+{
+    public GameObject HealEffekt;
+    float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.F) && timer >= 5)
+        {
+            Instantiate(HealEffekt);
+            timer = 0;
+        }
+    }
+
+
+}
